16d76a0d567ef38d90b8132d72fd10a64eb34ae8
Ronal <65000548+tra666@users.noreply.github.com>
2023-10-31 20:18:03 -0300

    Add files via upload

--- Ejercicios_Ressuelto.py
+++ Ejercicios_Ressuelto.py
@@ -46,8 +46,9 @@
 #     print("No debes pagar el impuesto.")
 
 
-#pide al usuario que introduzca su edad e ingresos mensuales. Luego, verifica si la edad es mayor o igual a 18 y si los ingresos son mayores o iguales a $200.000.
-#Si ambas condiciones son verdaderas, el programa imprime que el usuario debe pagar el impuesto. Si no, imprime que el usuario no debe pagar el impuesto.
+#Este programa pide al usuario que introduzca su edad e ingresos mensuales. Luego, verifica si la edad es mayor o igual a 18 y si los ingresos son mayores 
+# o iguales a $200.000. Si ambas condiciones son verdaderas, 
+# el programa imprime que el usuario debe pagar el impuesto. Si no, imprime que el usuario no debe pagar el impuesto.
 
 
 #5- Escribir un programa para una empresa de juegos para todas las edades y quiere calcular de forma automática el precio que debe cobrar a sus clientes 
@@ -55,49 +56,75 @@
 # gratis, si tiene entre 4 y 18 años debe pagar $500 y si es mayor de 18 años, $1000.
 
 
-edad = int(input("Por favor, introduce la edad del cliente: "))
+# edad = int(input("Por favor, introduce la edad del cliente: "))
 
-if edad < 4:
-    print("El cliente puede entrar gratis.")
-elif 4 <= edad <= 18:
-    print("El precio de la entrada es $500.")
-else:
-    print("El precio de la entrada es $1000.")
+# if edad < 4:
+#     print("El cliente puede entrar gratis.")
+# elif 4 <= edad <= 18:
+#     print("El precio de la entrada es $500.")
+# else:
+#     print("El precio de la entrada es $1000.")
 
 
-#pide al usuario que introduzca la edad del cliente. Luego, verifica si el cliente es menor de 4 años
+#Este programa pide al usuario que introduzca la edad del cliente. Luego, verifica si el cliente es menor de 4 años
 #entre 4 y 18 años, o mayor de 18 años. Dependiendo de la edad del cliente, el programa imprime el precio de la entrada correspondiente.
 
 
 #6- Escribir un programa que solicite una contrasena y esta se deba volver a confirmar, el programa terminara cuando ambas contrasenas coincidan.
 
-contrasena = input("Por favor, introduce una contraseña: ")
-confirmacion = input("Por favor, confirma tu contraseña: ")
+# contraseña = input("Por favor, introduce una contraseña: ")
+# confirmacion = input("Por favor, confirma tu contraseña: ")
 
-while contrasena != confirmacion:
-    print("Las contraseñas no coinciden. Inténtalo de nuevo.")
-    contrasena = input("Por favor, introduce una contraseña: ")
-    confirmacion = input("Por favor, confirma tu contraseña: ")
+# while contraseña != confirmacion:
+#     print("Las contraseñas no coinciden. Inténtalo de nuevo.")
+#     contraseña = input("Por favor, introduce una contraseña: ")
+#     confirmacion = input("Por favor, confirma tu contraseña: ")
 
-print("Las contraseñas coinciden. El programa ha terminado.")
+# print("Las contraseñas coinciden. El programa ha terminado.")
 
-#pide al usuario que introduzca una contraseña y luego la confirme. Si las contraseñas no coinciden
+
+#Este programa pide al usuario que introduzca una contraseña y luego la confirme. Si las contraseñas no coinciden
 # el programa le pide al usuario que lo intente de nuevo. Esto continúa hasta que las contraseñas
 #coinciden, momento en el cual el programa imprime un mensaje de confirmación y termina.
 
 
 
 
+# def solicitar_contrasena():
+#     while True:
+#         contrasena = input("Por favor, ingrese su contraseña: ")
+#         confirmacion = input("Por favor, confirme su contraseña: ")
+#         if contrasena == confirmacion:
+#             print("Las contraseñas coinciden. ¡Gracias!")
+#             break
+#         else:
+#             print("Las contraseñas no coinciden. Inténtalo de nuevo.")
+
+# solicitar_contrasena()
 
 
 
+#Este programa solicita al usuario que ingrese una contraseña y luego la confirme. Si las contraseñas no coinciden, 
+# el programa seguirá solicitando al usuario que ingrese y confirme la contraseña hasta que ambas coincidan. 
+# Una vez que las contraseñas coinciden, el programa imprime un mensaje de agradecimiento y termina.
+
+#True y break:
+
+#El True en el bucle while se utiliza para crear un bucle infinito. Esto significa que el bucle continuará ejecutándose hasta que se encuentre una 
+# instrucción break. El break se utiliza para salir del bucle. En este caso cuando las contraseñas ingresadas por el usuario coinciden, queremos salir del
+# bucle. Por eso usamos break. Entonces, en resumen, el bucle while True: continuará solicitando al usuario que ingrese y confirme una contraseña 
+# hasta que ambas contraseñas coincidan. En ese punto, se ejecuta la instrucción break, que termina el bucle. Después de eso
+# el programa continúa con cualquier código que esté después del bucle (si lo hay), o termina si no hay más código.
 
 
 
+# def solicitar_contrasena():
+#     while True:
 
-
-
-
-
+# Los dos puntos : en Python se utilizan para denotar el inicio de un bloque de código que está indentado. Este bloque de código puede ser el cuerpo
+# de una función, el cuerpo de un bucle, una declaración condicional, etc. En tu código, def solicitar_contrasena(): 
+# define una función llamada solicitar_contrasena. Los dos puntos : indican que el siguiente bloque de código indentado es el cuerpo de la función.
+# Del mismo modo, en la línea while True:, los dos puntos : indican que el siguiente bloque de código indentado se debe ejecutar 
+# mientras la condición después de while (en este caso, True) sea verdadera.
 
 
